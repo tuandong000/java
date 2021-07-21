2f339beb1db1867be7d19ae8c94a6b004dd06dc1
tuandong000 <81799170+tuandong000@users.noreply.github.com>
2021-07-21 15:25:01 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,38 +1,33 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exam
+namespace Ex1
 {
-    class Cylinder
+    class Show
     {
         static void Main(string[] args)
         {
-            Result();
-        }
-        static void Process(double Radius, double Height)
-        {
-            double BaseArea = Radius  Radius Math.PI;
-            double LateralArea = 2  Math.PI Radius *Height;
-            double TotalArea = 2  Math.PI Radius *(Height + Radius);
-            double Volume = Math.PI  Radius Radius *Height;
-            Console.WriteLine($"Base: {Math.Round(BaseArea, 2)}  | Lateral: {Math.Round(LateralArea, 2)}  | Total; {Math.Round(TotalArea, 2)}  | Volume: {Math.Round(Volume, 2)}");
-        }
+            string Name;
+            int Weight;
 
-        static void Result()
-        {
-            Console.WriteLine("Enter the dimenstions os the cylinder");
-            Console.WriteLine("Radius:");
-            double r = double.Parse(Console.ReadLine());
-            Console.WriteLine("Height:");
-            double h = double.Parse(Console.ReadLine());
-            Console.WriteLine("cylinder Characteristics");
-            Console.WriteLine($"Radius: {r}, Height: {h}");
-            Process(r, h);
-        }
+            Name = "tiger";
+            Weight = 100;
+            SetMe tiger = new SetMe(Name, Weight);
+            Console.WriteLine(tiger)
 
+;
+            Console.ReadLine();
+
+            Name = "lion";
+            Weight = 200;
+            SetMe lion = new SetMe(Name, Weight);
+            Console.WriteLine(lion)
+
+;
+            Console.ReadLine();
+        }
     }
 }
