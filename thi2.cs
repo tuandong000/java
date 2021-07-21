4aa3ee16cde2005a9739bf551d0963ce9d010fb0
tuandong000 <81799170+tuandong000@users.noreply.github.com>
2021-07-21 15:27:43 +0700

    Add files via upload

---
+++ thi2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    class SetMe
+    {
+        private string Name { get; set; }
+        private int Weight { get; set; }
+
+        public SetMe(string name, int weight)
+        {
+            this.Name = name;
+            this.Weight = weight;
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}, Weight: {Weight}";
+        }
+    }
+}
